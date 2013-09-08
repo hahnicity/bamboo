d986857957bf21091fcaad802bc6089ec84d4689
hahnicity <grehm87@gmail.com>
2013-09-07 23:57:17 -0700

    Heroku has weird permissions for flask apps, this could take awhile...

--- bamboo/main.py
+++ bamboo/main.py
@@ -3,6 +3,7 @@ bamboo.main
 ~~~~~~~~~~
 """
 from argparse import ArgumentParser
+from os import environ
 
 from bamboo.app import create_app
 from bamboo.configure import configure_app
@@ -24,7 +25,6 @@ def add_app_arguments(parser):
     """
     Add arguments for where the app will be run
     """
-    parser.add_argument("--port", help="The port to listen on", type=int)
     hosts = parser.add_mutually_exclusive_group()
     hosts.add_argument("--local", help="Launch the app on 127.0.0.1", action="store_true")
     hosts.add_argument("--host", help="Launch the app on a specific host eg: 1.1.1.1")
@@ -55,4 +55,4 @@ def main():
     app = create_app()
     with bamboo_context(engine=make_engine()):
         configure_app(app, args)
-        app.run(host=app.config["HOST"], port=args.port)
+        app.run(host=app.config["HOST"], port=environ.get("PORT", 5000))
