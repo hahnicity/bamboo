8bd35769786dea061084c06a950a484317da903e
hahnicity <grehm87@gmail.com>
2013-09-08 00:54:17 -0700

    stuff..

--- bamboo/main.py
+++ bamboo/main.py
@@ -2,22 +2,19 @@
 bamboo.main
 ~~~~~~~~~~
 """
-""""
 from argparse import ArgumentParser
-"""
 from os import environ
 
 from bamboo.app import create_app
-"""
 from bamboo.configure import configure_app
 from bamboo.connection import make_engine
 from bamboo.context import bamboo_context
 
 
 def build_parser():
-"""
+    """
     #Parse all command line arguments
-"""
+    """
     parser = ArgumentParser()
     add_app_arguments(parser)
     add_other_arguments(parser)
@@ -25,18 +22,18 @@ def build_parser():
 
 
 def add_app_arguments(parser):
-"""
+    """
     #Add arguments for where the app will be run
-"""
+    """
     hosts = parser.add_mutually_exclusive_group()
     hosts.add_argument("--local", help="Launch the app on 127.0.0.1", action="store_true")
     hosts.add_argument("--host", help="Launch the app on a specific host eg: 1.1.1.1")
 
 
 def add_other_arguments(parser):
-"""
+    """
     #Add other arguments
-"""
+    """
     others = parser.add_argument_group("Other Options")
     others.add_argument(
         "--debug",
@@ -49,26 +46,23 @@ def add_other_arguments(parser):
         action="store_true",
     )
 
-"""
-def debug_controllers(app):
-    @app.route("/login")
-    def stuff():
-        return "Hello World"
+
+#def debug_controllers(app):
+#    @app.route("/login")
+#    def stuff():
+#        return "Hello World"
 
 
 def main():
     """
     Console Entry point
     """
-    #args = build_parser().parse_args()
-    app = create_app()
-    debug_controllers(app)
-    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
+    with bamboo_context(engine=make_engine()):
+        args = build_parser().parse_args()
+        app = create_app()
+        configure_app(app, args)
+        app.run(host=app.config["HOST"], port=int(environ.get("PORT", 5000)))
 
-    #with bamboo_context(engine=make_engine()):
-    #    configure_app(app, args)
-    #    app.run(host=app.config["HOST"], port=environ.get("PORT", 5000))
-    #
 
 
 if __name__ == "__main__":
